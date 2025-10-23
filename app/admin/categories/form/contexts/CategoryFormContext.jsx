6e38262ffbffeefabdc01f5d89eaa38841605174
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-10-23 13:11:16 +0500

    Category context and layout created

---
+++ app/admin/categories/form/contexts/CategoryFormContext.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const CategoryFormContext = createContext();
+
+export default function CategoryFormContextProvider({ children }) {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isDone, setIsDone] = useState(false);
+
+  const handleData = (key, value) => {
+    setData({
+      ...data,
+      [key]: value,
+    });
+  };
+
+  const handleCreate = async () => {
+    setError(null);
+    setIsLoading(true);
+    setIsDone(false);
+    try {
+      setIsDone(true);
+    } catch (error) {
+      setError(error);
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <CategoryFormContext.Provider
+      value={{ data, isLoading, error, isDone, handleData, handleCreate }}
+    >
+      {children}
+    </CategoryFormContext.Provider>
+  );
+}
+
+export const useCategoryForm = () => useContext(CategoryFormContext);
